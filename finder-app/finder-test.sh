4308fdd288c8102f7328d154aa9836435e7ad1ba
Meenakshi Venkataraman <varshini_venkat@ymail.com>
2025-04-06 10:55:29 +0800

    finder-test-sh-assignment-4

--- finder-app/finder-test.sh
+++ finder-app/finder-test.sh
@@ -7,8 +7,8 @@ set -u
 
 NUMFILES=10
 WRITESTR=AELD_IS_FUN
-WRITEDIR=/tmp/aeld-data
-username=$(cat conf/username.txt)
+WRITEDIR=/tmp/
+username=$(cat /etc/conf/username.txt)
 
 if [ $# -lt 3 ]
 then
@@ -22,7 +22,7 @@ then
 else
 	NUMFILES=$1
 	WRITESTR=$2
-	WRITEDIR=/tmp/aeld-data/$3
+	WRITEDIR=/tmp/$3
 fi
 
 MATCHSTR="The number of files are ${NUMFILES} and the number of matching lines are ${NUMFILES}"
@@ -32,7 +32,7 @@ echo "Writing ${NUMFILES} files containing string ${WRITESTR} to ${WRITEDIR}"
 rm -rf "${WRITEDIR}"
 
 # create $WRITEDIR if not assignment1
-assignment=`cat conf/assignment.txt`
+assignment=`cat /etc/conf/assignment.txt`
 
 if [ $assignment != 'assignment1' ]
 then
@@ -71,3 +71,5 @@ else
 	echo "failed: expected  ${MATCHSTR} in ${OUTPUTSTRING} but instead found"
 	exit 1
 fi
+
+echo $OUTPUTSTRING>/tmp/assignment4-result.txt
